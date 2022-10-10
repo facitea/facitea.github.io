b66e3f793d149df409b862ada6b3c39efcbb1ec8
Facite <89388117+facitea@users.noreply.github.com>
2022-10-10 13:14:47 +0900

    22/10/10

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDU5hIzUSfHwHgPxsvRdH6QkLM-_8jaekU",
+    authDomain: "hawktalk-b0a71.firebaseapp.com",
+    databaseURL: "https://hawktalk-b0a71-default-rtdb.firebaseio.com",
+    projectId: "hawktalk-b0a71",
+    storageBucket: "hawktalk-b0a71.appspot.com",
+    messagingSenderId: "537777830041",
+    appId: "1:537777830041:web:1b5ce3e943d55c09d1e008",
+    measurementId: "G-7T0M3EK19Q"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
