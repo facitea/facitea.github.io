8409fa907ec0db120f9183e0346081f1eaaa9d1c
Facite <89388117+facitea@users.noreply.github.com>
2022-10-11 21:49:02 +0900

    22/10/11
    Mypage삽입 및 모달창으로 로그인, 회원가입 구현 중. firebase 인증 유지 기능 구현 필요.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+const auth = firebase.auth(); //구글은 나중에 하고 이메일 가입만 일단 허용
 
 db.collection("users").add({
     first: "Ada",
@@ -22,3 +23,26 @@ db.collection("users").add({
 .catch((error) => {
     console.error("Error adding document: ", error);
 });
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // 가입
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // 로그인
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
